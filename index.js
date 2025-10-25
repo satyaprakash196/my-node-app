a8a082ff03585cbfd277c77711b459d20fc9300b
satyaprakash196 <131759884+satyaprakash196@users.noreply.github.com>
2025-10-25 15:53:52 +0530

    Update index.js.2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Dockerized Node.js app-ci-cd!\n');
+  res.end('Hello from Dockerized Node.js app-ci-cd-satya!\n');
 });
 
 server.listen(PORT, () => {
