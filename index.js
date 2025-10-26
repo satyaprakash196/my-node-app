23a755d67e9d6c7b1edcddd13f90922b4b863323
satyaprakash196 <131759884+satyaprakash196@users.noreply.github.com>
2025-10-26 23:32:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Dockerized Node.js app-ci-cd-satya-dev-test-prod!\n');
+  res.end('Hello from Dockerized Node.js app-ci-cd-satya-dev-test-prod-intrigate to webhook!\n');
 });
 
 server.listen(PORT, () => {
